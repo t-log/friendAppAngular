a3f2971b4555d84bd40862cd7c72cd5f7b6e0092
akhil <akhil.paul@ymail.com>
2022-11-26 12:19:59 +0530

    Adds addFriend and navbar feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddFriendsComponent } from './add-friends/add-friends.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const myRoute:Routes = [{path:"",component:AddFriendsComponent},
+                        {path:"add",component:AddFriendsComponent},
+                        
+
+
+                       ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddFriendsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]

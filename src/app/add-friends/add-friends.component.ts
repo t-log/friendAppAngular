4dac3353064cf3f54bf803c30b672eda7ea8f674
akhil <akhil.paul@ymail.com>
2022-11-28 10:02:40 +0530

    Adds two way binding

--- src/app/add-friends/add-friends.component.ts
+++ src/app/add-friends/add-friends.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-friends.component.css']
 })
 export class AddFriendsComponent {
+  name=""
+  friendName=""
+  DescribeYourFriend=""
+  friendNickName=""
 
 }

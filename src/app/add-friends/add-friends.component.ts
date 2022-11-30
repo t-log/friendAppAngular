3cf25d4cfaa7704ba031d4313a4d384058f5318c
akhil <akhil.paul@ymail.com>
2022-11-30 10:19:43 +0530

    Adds alerts for checking response

--- src/app/add-friends/add-friends.component.ts
+++ src/app/add-friends/add-friends.component.ts
@@ -18,7 +18,17 @@ export class AddFriendsComponent {
     let data:any = {"name":this.name,"friendName":this.friendName,"friendNickName":this.friendNickName,"DescribeYourFriend":this.DescribeYourFriend}
     console.log(data)
     this.api.addFriend(data).subscribe(
-      (response)=>{console.log(response)}
+      (response:any)=>{
+        if (response.status=="success") {
+          alert("Record added successfully")
+          this.name=""
+          this.friendName=""
+          this.friendNickName=""
+          this.DescribeYourFriend=""
+        } else {
+          alert("Something went wrong!")
+        }
+      }
     )
   }
   
